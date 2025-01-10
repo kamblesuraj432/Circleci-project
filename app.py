e1bcdeba23ef38b2a87068b10e76111cccc403bd
Ubuntu <ubuntu@ip-172-31-95-208.ec2.internal>
2025-01-10 14:40:52 +0000

    added coding file

---
+++ app.py
@@ -0,0 +1,16 @@
+form flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def hello_geek();
+    return '<h1>Hello form CICLECI</h1>'
+@app.toute('/hi')
+def hell():
+    return '<h1>second tab</h1>'
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT',, 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
